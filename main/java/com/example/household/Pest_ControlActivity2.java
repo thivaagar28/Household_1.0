6309207d7e370685f7240c37caa422c05a4e325e
thivaagar28 <94023077+thivaagar28@users.noreply.github.com>
2022-01-30 18:39:06 +0800

    Update Pest_ControlActivity2.java

--- main/java/com/example/household/Pest_ControlActivity2.java
+++ main/java/com/example/household/Pest_ControlActivity2.java
@@ -3,9 +3,7 @@ package com.example.household;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-
-public class Pest_ControlActivity2 extends AppCompatActivity {
-
+public class Pest_ControlActivity2 extends AppCompatActivity{
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
