4f63a25a2c3ddb09470588bfdf784df49822495b
Yeganathan <95484796+Yeganathan@users.noreply.github.com>
2022-01-30 17:00:15 +0800

    Update Pest_ControlActivity2.java

--- main/java/com/example/household/Pest_ControlActivity2.java
+++ main/java/com/example/household/Pest_ControlActivity2.java
@@ -9,6 +9,6 @@ public class Pest_ControlActivity2 extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_pest_control2);
+        setContentView(R.layout.activity_pest_control2); // UI layout file for the pest control service
     }
 }
