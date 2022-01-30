ea742b0f5a437efc7e9647601a01ddb67382cf2d
Yeganathan <95484796+Yeganathan@users.noreply.github.com>
2022-01-30 16:57:19 +0800

    Update House_Cleaning_Activity2.java

--- main/java/com/example/household/House_Cleaning_Activity2.java
+++ main/java/com/example/household/House_Cleaning_Activity2.java
@@ -9,6 +9,6 @@ public class House_Cleaning_Activity2 extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_house_cleaning2);
+        setContentView(R.layout.activity_house_cleaning2);// UI layout for the house cleaning service
     }
 }
